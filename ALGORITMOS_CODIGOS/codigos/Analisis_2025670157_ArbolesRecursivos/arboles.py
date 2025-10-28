b24c271f190fbc336f322551467d0cdb8b3f9247
rubyesmelc0911 <rubyesme.lc0911@gmail.com>
2025-10-28 08:04:43 -0600

    JAVA Ejercicios y Trabajo en Arboles

---
+++ ALGORITMOS_CODIGOS/codigos/Analisis_2025670157_ArbolesRecursivos/arboles.py
@@ -0,0 +1,33 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+# función para insertar valores en el árbol
+def insertar(raiz, valor):
+    if raiz is None:
+        return Nodo(valor)
+    if valor < raiz.valor:
+        raiz.izquierda = insertar(raiz.izquierda, valor)
+    else:
+        raiz.derecha = insertar(raiz.derecha, valor)
+    return raiz
+
+def imprimir_arbol(raiz, nivel=0, lado="R"):
+    if raiz is not None:
+        imprimir_arbol(raiz.derecha, nivel + 1, "Derecha")
+        print("   " * nivel + f"({lado})--> {raiz.valor}")
+        imprimir_arbol(raiz.izquierda, nivel + 1, "Izquierda")
+
+def main():
+    raiz = None
+    valores = [50, 30, 70, 20, 40, 60, 80]
+    # insertar valores
+    for valor in valores:
+        raiz = insertar(raiz, valor)
+    
+    imprimir_arbol(raiz)
+
+if __name__ == "__main__":
+    main()
---
+++ ALGORITMOS_CODIGOS/codigos/Analisis_2025670157_ArbolesRecursivos/arboles2.py
@@ -0,0 +1,7 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+        #numero de hojas
